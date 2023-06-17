2bd5b2c17372103a57e4965b9a3813d00976dd7a
Kingsley Botchway <kingsleybotchwayedu11@localhost.localdomain>
2023-06-17 05:03:12 -0400

    kofi

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,9 +1,8 @@
-import Car from './10-car';
-
-export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this._range = range;
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   static get [Symbol.species]() {
